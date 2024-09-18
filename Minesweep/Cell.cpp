6e4f4965e32575c30aab111c00053f8d1fa4ec40
Yoe <dembouz719@gmail.com>
2024-09-18 11:38:33 +0200

    created the getDisplayChar metod

--- Minesweep/Cell.cpp
+++ Minesweep/Cell.cpp
@@ -10,3 +10,24 @@ bool Cell::isRevealed() const {
 	return revealed; 
 }
 
+bool Cell::isMine() const {
+	return mine;
+}
+
+void Cell::reveal() {
+	revealed = true;
+}
+// sets Mint to has mine
+void Cell::setMine(bool hasMine) {
+	mine = hasMine;
+}
+// get Display char return a character based on the cell conditions
+char Cell::getDisplayChar() const {
+	if (!isRevealed) {
+		return '+';
+	} else if (mine) {
+		return '*';
+	} else {
+		return ' ';
+	}
+}

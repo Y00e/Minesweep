010b86a293bebaff705c0313d00ab872c4b2f61e
Yoe <dembouz719@gmail.com>
2024-09-19 23:01:58 +0200

    Created Game rows, cols, mines and called for game.play metod

--- Minesweep/Minesweep.cpp
+++ Minesweep/Minesweep.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "Board.h"
+#include "Game.h"
+
+
+const int ROWS = 10;
+const int COLS = 10;
+const int MINES = 15;
 
 int main()
-{
+{   
+    
+    
     std::cout << "Minesweep Start Game!\n";
+    
+    Game game(ROWS, COLS, MINES);
+    game.play();
+
+    return 0;
 }
 

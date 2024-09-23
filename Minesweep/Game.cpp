4235121660b621c39209cfbbc0290e860a87ea62
Yoe <dembouz719@gmail.com>
2024-09-23 20:04:41 +0200

    Added handled user input to toggle flag or reveal cell based on action

--- Minesweep/Game.cpp
+++ Minesweep/Game.cpp
@@ -12,11 +12,13 @@ Game::Game(int rows, int cols, int mines) : board(rows, cols) {
 void Game::play() {
     while (true) {
         board.printBoard();
-        
+
         char rowChar;
         int col;
-        std::cout << "Enter the row and column (ex, a 3): ";
-        std::cin >> rowChar >> col;
+        char action;
+
+        std::cout << "Enter action (R for reveal, F for flag), row and column (ex, R a 3): ";
+        std::cin >> action >> rowChar >> col;
 
         if (std::cin.fail()) {
             std::cin.clear(); // clear the error status
@@ -24,20 +26,29 @@ void Game::play() {
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         // Adjust for 0-based indexing
-        int x = rowChar - 'a';
+        int x = std::toupper(rowChar) - 'A';
 
         int y = col - 1;
+
 
         if (!board.isValid(x, y)) {
             std::cout << "Invalid move! Try again." << std::endl;
             continue;
         }
 
-        if (board.revealCell(x, y)) {
+        
+        action = std::toupper(action);
+        if (action == 'F') {
+            board.toggleFlag(x, y);
+        }
+        else if (action == 'R') {
+            if (board.revealCell(x, y)) {
             std::cout << "Game Over! You stepped on a mine." << std::endl;
             board.printBoard();
             return;
         }
+        }
+       
 
         if (board.isGameWon()) {
             std::cout << "Congratulations! You won the game." << std::endl;
